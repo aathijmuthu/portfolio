4e1e42df91872a1456f736e294b5c67ab07b7674
aathijmuthu <65832367+aathijmuthu@users.noreply.github.com>
2025-04-18 17:44:01 -0700

    finished step 3 and part of 4

--- global.js
+++ global.js
@@ -3,3 +3,62 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// const navLinks = $$('nav a');
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink?.classList.add('current');
+
+// console.log('currentLink', currentLink);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/portfolio/";         // GitHub Pages repo name
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/aathijmuthu', title: 'GitHub Profile' },
+  // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+  );
+  if (a.host != location.host) {
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+  }
+  nav.append(a);
+}
+
+// Add Dark Mode Toggle
+document.body.insertAdjacentHTML(
+  "afterbegin",
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+      <option value="light dark">Auto</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+		</select>
+	</label>`
+);
